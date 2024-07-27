ceef7cb914e27ee77e6a6032c4d0658ba2111fc9
Siam11561 <siam11651@outlook.com>
2024-07-27 22:28:35 +0600

    kaaj kori

--- include/json/object.hpp
+++ include/json/object.hpp
@@ -17,6 +17,7 @@ namespace json
     public:
         object();
         const value *operator[](const std::string &name) const;
+        const value *operator[](const std::string &&name) const;
         ~object();
     };
 }
